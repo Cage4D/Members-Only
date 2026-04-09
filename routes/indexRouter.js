a1d839b48ca5ed128bcb71c94eb6633d85d06744
Johnny Cage <thespammingbott@gmail.com>
2026-04-09 10:10:28 +0100

    feat: show messages with poster username or 'Anonymous'

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,15 @@
 const { Router } = require("express")
 const indexRouter = Router()
+const pool = require("../data/pool")
 
-indexRouter.get("/", (req, res) => {
-    res.render("home", { user: req.user })
+indexRouter.get("/", async (req, res, next) => {
+    try {
+        const result = await pool.query("SELECT * FROM messages")
+        const messages = result.rows
+        res.render("home", { user: req.user, messages, })
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = indexRouter;
